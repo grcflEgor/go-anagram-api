7aabd8b55056fe95fe077bbcec977fa6ee478035
Egor Dmitrienko <e.dmitrienko@oe-media.ru>
2025-08-27 14:39:56 +0300

    feat: добавлен базовый  usecase слой

---
+++ internal/usecase/anagram.go
@@ -0,0 +1,43 @@
+package usecase
+
+import (
+	"context"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/grcflEgor/go-anagram-api/internal/domain"
+	"github.com/grcflEgor/go-anagram-api/internal/repository"
+)
+
+type AnagramUseCase struct {
+	repo repository.TaskRepository
+	taskQueue chan<- *domain.Task
+}
+
+func NewAnagramUseCase(repo repository.TaskRepository, taskQueue chan<- *domain.Task) *AnagramUseCase {
+	return &AnagramUseCase{
+		repo: repo,
+		taskQueue: taskQueue,
+	}
+}
+
+func (uc *AnagramUseCase) CreateTask(ctx context.Context, words []string) (string, error) {
+	task := &domain.Task{
+		ID: uuid.New().String(),
+		Status: domain.StatusProcessing,
+		Words: words,
+		CreatedAt: time.Now(),
+	}
+
+	if err := uc.repo.Save(ctx, task); err != nil {
+		return "", err
+	}
+
+	uc.taskQueue <- task
+
+	return task.ID, nil
+}
+
+func (uc *AnagramUseCase) GetTaskByID(ctx context.Context, id string) (*domain.Task, error) {
+	return uc.repo.GetByID(ctx, id)
+}
